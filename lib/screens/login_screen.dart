feaa29b0e583eadd3d312530b8208c318bf358c9
Joakim Waltersson <Joakim.waltersson@gmail.com>
2020-08-09 08:50:43 +0200

    Add named routes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
+  static final String id = 'login_screen';
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
